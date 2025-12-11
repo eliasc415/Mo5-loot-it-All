bf3535f7bfc7156090ca588defd226b7fb367bad
Elias <Elcabrera1@s.sfusd.edu>
2025-12-11 11:58:44 -0800

    M5L9

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -37,12 +37,12 @@ func _process(delta: float) -> void:
 
 func set_gem_count(new_gem_count: int) -> void:
 	gem_count = new_gem_count
-	get_node("UI/GemCount").text = "x" + str(gem_count)
+	get_node("UI/Label").text = "x" + str(gem_count)
 
 
 func set_health(new_health: int) -> void:
 	health = new_health
-	get_node("UI/HealthBar").value = health
+	get_node("UI/ProgressBar").value = health
 
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
